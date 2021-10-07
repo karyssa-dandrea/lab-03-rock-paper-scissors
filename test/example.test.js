8d0b749e95af0acb8331cccee45521d16a5c6be8
karyssa-dandrea <karyssadandrea@gmail.com>
2021-10-06 17:53:04 -0700

    finished my testing

--- test/example.test.js
+++ test/example.test.js
@@ -26,3 +26,33 @@ test('user: paper, computer: scissors', (expect) => {
     const actual = didUserWin('paper', 'scissors');
     expect.equal(actual, expected);
 });
+
+test('user: scissors, computer: paper', (expect) => {
+    const expected = 'wins';
+    const actual = didUserWin('scissors', 'paper');
+    expect.equal(actual, expected);
+});
+
+test('user: scissors, computer: rock', (expect) => {
+    const expected = 'losses';
+    const actual = didUserWin('scissors', 'rock');
+    expect.equal(actual, expected);
+});
+
+test('user: scissors, computer: scissors', (expect) => {
+    const expected = 'draws';
+    const actual = didUserWin('scissors', 'scissors');
+    expect.equal(actual, expected);
+});
+
+test('user: paper, computer: paper', (expect) => {
+    const expected = 'draws';
+    const actual = didUserWin('paper', 'paper');
+    expect.equal(actual, expected);
+});
+
+test('user: rock, computer: rock', (expect) => {
+    const expected = 'draws';
+    const actual = didUserWin('rock', 'rock');
+    expect.equal(actual, expected);
+});
--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
 export function didUserWin(userChoice, actualDraw){
      if (userChoice === actualDraw){
-         return 'It\'s a draw!'
+         return 'draws'
      } else if (userChoice === 'rock' && actualDraw === 'scissors') {
          return 'wins';
      } else if (userChoice === 'rock' && actualDraw === 'paper') {
@@ -9,9 +9,21 @@ export function didUserWin(userChoice, actualDraw){
     return 'wins';
 } else if (userChoice === 'paper' && actualDraw === 'scissors') {
     return 'losses';
+} else if (userChoice === 'scissors' && actualDraw === 'paper') {
+    return 'wins';
+} else if (userChoice === 'scissors' && actualDraw === 'rock') {
+    return 'losses';
+} else if (userChoice === 'scissors' && actualDraw === 'scissors') {
+    return 'draws';
+} else if (userChoice === 'paper' && actualDraw === 'paper') {
+    return 'draws';
 
+} else if (userChoice === 'rock' && actualDraw === 'rock') {
+    return 'draws';
 }
 }
+
+
 export function getRandomThrow(){
     return ['rock', 'paper', 'scissors'][Math.round(Math.random())];
 }
