d7f3c0482b05b8babf7a91edc88eb2ae5c1da253
karyssa-dandrea <karyssadandrea@gmail.com>
2021-10-08 15:47:16 -0700

    fixed my bugs

--- app.js
+++ app.js
@@ -7,16 +7,9 @@ const lossSpan = document.getElementById('losses');
 const draw = document.getElementById('draws');
 const error = document.getElementById('error');
 
-
-// initialize global state
 let wins = 0;
 let losses = 0;
 let draws = 0;
-
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
 
   playButton.addEventListener('click', ()=>{
     const selected = document.querySelector('input[type=radio]:checked');
@@ -24,23 +17,15 @@ let draws = 0;
     if(!selected){
       return error.classList.remove('hidden');
     }
+    console.log(selected);
     error.classList.add('hidden');
     const userThrow = selected.value;
     const computerThrow = getRandomThrow();
-    //console.log('userThrow');
-    //console.log('computerThrow');
-    
-  
 
-    if(didUserWin(userThrow, computerThrow)) wins++;
-    if(!didUserWin(userThrow, computerThrow)) losses++;
-    if(userThrow === computerThrow){
-      draws++;
-      console.log('it was a draw');
-    } 
-      
+    if(didUserWin(userThrow, computerThrow) === true) wins++;
+    if(didUserWin(userThrow, computerThrow) === false) losses++;
+    if(didUserWin(userThrow, computerThrow) === 'draw') draws++;
     
-  
     winSpan.textContent = wins;
     lossSpan.textContent = losses;
     draw.textContent = draws;
--- utils.js
+++ utils.js
@@ -1,29 +1,22 @@
 export function didUserWin(userChoice, actualDraw){
-     if (userChoice === actualDraw){
-         return 'draws'
-     } else if (userChoice === 'rock' && actualDraw === 'scissors') {
-         return 'wins';
-     } else if (userChoice === 'rock' && actualDraw === 'paper') {
-        return 'losses';
-} else if (userChoice === 'paper' && actualDraw === 'rock') {
-    return 'wins';
-} else if (userChoice === 'paper' && actualDraw === 'scissors') {
-    return 'losses';
-} else if (userChoice === 'scissors' && actualDraw === 'paper') {
-    return 'wins';
-} else if (userChoice === 'scissors' && actualDraw === 'rock') {
-    return 'losses';
-} else if (userChoice === 'scissors' && actualDraw === 'scissors') {
-    return 'draws';
-} else if (userChoice === 'paper' && actualDraw === 'paper') {
-    return 'draws';
-
-} else if (userChoice === 'rock' && actualDraw === 'rock') {
-    return 'draws';
+    console.log(userChoice, actualDraw);
+    if (userChoice === actualDraw){
+        return 'draw';
+    } else if (userChoice === 'rock' && actualDraw === 'scissors') {
+        return true;
+    } else if (userChoice === 'rock' && actualDraw === 'paper') {
+        return false;
+    } else if (userChoice === 'paper' && actualDraw === 'rock') {
+        return true;
+    } else if (userChoice === 'paper' && actualDraw === 'scissors') {
+        return false;
+    } else if (userChoice === 'scissors' && actualDraw === 'paper') {
+        return true;
+    } else if (userChoice === 'scissors' && actualDraw === 'rock') {
+        return false;
+    }
 }
-}
-
 
 export function getRandomThrow(){
-    return ['rock', 'paper', 'scissors'][Math.round(Math.random())];
+    return ['rock', 'paper', 'scissors'][Math.floor(Math.random() * 3)];
 }
