180b2ce8447353a9fe5b0c043a8db64ddf0ca6b8
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-10-08 16:31:29 -0700

    incremented reset button

--- app.js
+++ app.js
@@ -7,10 +7,13 @@ const lossSpan = document.getElementById('losses');
 const draw = document.getElementById('draws');
 const error = document.getElementById('error');
 const resetButton = document.getElementById('reset');
+const resetDisplay = document.getElementById('reset-display');
 
 let wins = 0;
 let losses = 0;
 let draws = 0;
+let resets = 0;
+
 
   playButton.addEventListener('click', ()=>{
     const selected = document.querySelector('input[type=radio]:checked');
@@ -33,10 +36,12 @@ let draws = 0;
   })
 
   resetButton.addEventListener('click', () => {
+    resets++; 
     wins = 0;
     losses = 0;
     draws = 0;
     winSpan.textContent = wins;
     lossSpan.textContent = losses;
     draw.textContent = draws;
+    resetDisplay.textContent = resets;
   })
